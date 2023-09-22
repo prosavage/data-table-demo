d3fd07bb229800761b29d145ff5fb54e5e0f290f
Naman <naman@flexpool.io>
2023-09-21 17:01:29 -0700

    Change title

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -6,7 +6,7 @@ interface TitleProps {
 
 export const Title: React.FC<TitleProps> = ({ }) => {
       return <div className="p-3 sm:p-0">
-            <p className="text-2xl font-bold">Swish Analytics Technical Assessment</p>
+            <p className="text-2xl font-bold">Data Table Demo</p>
             <p className="text-md font-medium">Naman Gupta</p>
       </div>;
 }
